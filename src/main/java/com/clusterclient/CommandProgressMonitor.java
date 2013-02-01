0cbc2889e22814847f5d4495bd06796bab6a1e81
Jarlath Holleran <jholleran@gmail.com>
2013-02-01 13:57:44 +0000

    JavaDoc update

--- src/main/java/com/clusterclient/CommandProgressMonitor.java
+++ src/main/java/com/clusterclient/CommandProgressMonitor.java
@@ -1,10 +1,33 @@
 package com.clusterclient;
 
+/**
+ * This interface is used to implement a progress monitor for the user when long
+ * commands are being run.
+ * 
+ * @author HolleranJ
+ * 
+ */
 public interface CommandProgressMonitor {
 
-	  void init(String display, long max);
+	/**
+	 * Initials the progress monitor.
+	 * 
+	 * @param display
+	 * @param max
+	 */
+	void init(String display, long max);
 
-	  boolean count(long count);
+	/**
+	 * This method can be used to track how much of the command has been
+	 * completed.
+	 * 
+	 * @param count
+	 * @return
+	 */
+	boolean count(long count);
 
-	  void end();
+	/**
+	 * This method is called when the command being run has ended.
+	 */
+	void end();
 }
