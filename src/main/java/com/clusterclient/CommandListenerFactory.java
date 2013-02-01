63deb0474e567395cd58fb21c35f27c3b85e7f0e
Jarlath Holleran <jholleran@gmail.com>
2013-02-01 13:43:57 +0000

    Added JavaDoc

--- src/main/java/com/clusterclient/CommandListenerFactory.java
+++ src/main/java/com/clusterclient/CommandListenerFactory.java
@@ -1,8 +1,34 @@
 package com.clusterclient;
 
+/**
+ * This interface is used to create listeners for the {@link CommandService}'s
+ * 
+ * @author HolleranJ
+ * 
+ */
 public interface CommandListenerFactory {
 
-	TextListener makeTextListener(CommandService service, String host, String mode);
+	/**
+	 * Make a text listener for the {@link CommandService}. The listener will
+	 * need to be added to the {@link CommandService}.
+	 * 
+	 * @param service
+	 * @param host
+	 * @param mode
+	 * @return
+	 */
+	TextListener makeTextListener(CommandService service, String host,
+			String mode);
 
-	FileStructureListener makeFileNavigationListener(CommandService service, String host, String mode);
+	/**
+	 * Makes a listener that can display system file structure to the user. The
+	 * Listener will need to be added to the {@link CommandService}.
+	 * 
+	 * @param service
+	 * @param host
+	 * @param mode
+	 * @return
+	 */
+	FileStructureListener makeFileNavigationListener(CommandService service,
+			String host, String mode);
 }
