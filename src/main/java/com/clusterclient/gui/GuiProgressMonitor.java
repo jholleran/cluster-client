bae833e2b56987f46c9f11320db0e1dcaa164a93
Jarlath Holleran <jholleran@gmail.com>
2013-02-12 13:40:21 +0000

    code tidyup

--- src/main/java/com/clusterclient/gui/GuiProgressMonitor.java
+++ src/main/java/com/clusterclient/gui/GuiProgressMonitor.java
@@ -1,50 +1,49 @@
 package com.clusterclient.gui;
 
+import com.clusterclient.CommandProgressMonitor;
 import java.awt.Component;
-
 import javax.swing.ProgressMonitor;
 
-import com.clusterclient.CommandProgressMonitor;
-
 public class GuiProgressMonitor implements CommandProgressMonitor {
-	
-	private final Component parent;
-	private ProgressMonitor monitor;
-	private long count = 0;
-	private long max = 0;
-	private long percent = -1;
 
-	public GuiProgressMonitor(Component parent) {
-		this.parent = parent;
-	}
-	
-	public void init(String displayStr, long max) {
-		this.max = max;
-		monitor = new ProgressMonitor(parent, displayStr,
-				"", 0, (int) max);
-		count = 0;
-		percent = -1;
-		monitor.setProgress((int) this.count);
-		monitor.setMillisToDecideToPopup(100);
-	}
+    private final Component parent;
+    private ProgressMonitor monitor;
+    private long count = 0;
+    private long max = 0;
+    private long percent = -1;
 
-	public boolean count(long count) {
+    public GuiProgressMonitor(Component parent) {
+        this.parent = parent;
+    }
 
-		this.count += count;
+    @Override
+    public void init(String displayStr, long max) {
+        this.max = max;
+        monitor = new ProgressMonitor(parent, displayStr,
+                "", 0, (int) max);
+        count = 0;
+        percent = -1;
+        monitor.setProgress((int) this.count);
+        monitor.setMillisToDecideToPopup(100);
+    }
 
-		if (percent >= this.count * 100 / max) {
-			return true;
-		}
-		percent = this.count * 100 / max;
+    public boolean count(long count) {
 
-		monitor.setNote("Completed " + this.count + "(" + percent
-				+ "%) out of " + max + ".");
-		monitor.setProgress((int) this.count);
+        this.count += count;
 
-		return !(monitor.isCanceled());
-	}
+        if (percent >= this.count * 100 / max) {
+            return true;
+        }
+        percent = this.count * 100 / max;
 
-	public void end() {
-		monitor.close();
-	}
+        monitor.setNote("Completed " + this.count + "(" + percent
+                + "%) out of " + max + ".");
+        monitor.setProgress((int) this.count);
+
+        return !(monitor.isCanceled());
+    }
+
+    public void end() {
+        monitor.close();
+    }
 }
